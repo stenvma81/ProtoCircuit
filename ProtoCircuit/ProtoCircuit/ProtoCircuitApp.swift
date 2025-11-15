88c0122b09b1f967cc297981bc5ca4fe85a71268
Matti Stenvall <matti.stenvall@gmail.com>
2025-11-15 13:45:43 +0200

    Add app foundation, .gitignore

---
+++ ProtoCircuit/ProtoCircuit/ProtoCircuitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProtoCircuitApp.swift
+//  ProtoCircuit
+//
+//  Created by Matti Stenvall on 15.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct ProtoCircuitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
